eb8c34d49b8406139e1ca9ec0f25a1514b883f61
devshiva619 <deveshverma619@gmail.com>
2018-12-27 20:27:12 +0530

    new-website initial commit

---
+++ js/signup.js
@@ -0,0 +1,6 @@
+function showsignup() {
+  var i = document.getElementsByClassName("main_content");
+  var x = document.getElementsByClassName("signup");
+  i[0].style.display = "none";
+  x[0].style.display = "block";
+}
