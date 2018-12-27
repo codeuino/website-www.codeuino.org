425bd9f0ad3cb46e08257833d5764e52196d037e
Devesh Verma <deveshverma619@gmail.com>
2018-12-27 20:21:53 +0530

    Revert "new-website initial commit"

--- js/signup.js
+++
@@ -1,6 +0,0 @@
-function showsignup() {
-  var i = document.getElementsByClassName("main_content");
-  var x = document.getElementsByClassName("signup");
-  i[0].style.display = "none";
-  x[0].style.display = "block";
-}
