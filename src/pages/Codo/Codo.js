a1fc394d12c879b31240e7f6a1c5df9569d1d195
ayushnagar123 <an431999@gmail.com>
2020-01-25 00:37:53 +0530

    updated website with react framework

---
+++ src/pages/Codo/Codo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Codo() {
+  return (
+    <>
+        <h1>Codo</h1>
+    </>
+  );
+}
+ Codo.propTypes = {
+
+}
---
+++ src/pages/JoinUs/JoinUs.js
@@ -0,0 +1,127 @@
+import React from 'react';
+
+const JoinUs = () => {
+    return (
+        <div>
+            <div class="mt-12"></div>
+        <div class="container content blog-page blog-item" id="getInvolved-content">
+            <div class="blog margin-bottom-40" id="donut">
+                <div class="g-heading-v9 g-mb-30">
+                <h2><strong>Get Involved</strong></h2>
+                    <h3 class="text-uppercase">Welcome to Newcomers !!!</h3>
+                </div>
+                <section class="container">
+                    <div class="row" style={{display:"flex",justifyContent:"space-evenly"}}>
+                        <a href="http://slack.codeuino.org"><img src="https://cdn0.iconfinder.com/data/icons/tuts/256/slack_alt.png"/></a>
+                        <a href="https://github.com/codeuino"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUGABB70TILWLV3BLkJ0hLr0arXG20T7pGd6czWU7tzdkwdoRV&s"/></a>
+                    </div>
+                    <h3>Join us with</h3>
+                </section>
+                <p class="g-fs-16 ta-js">Codeuino is a non-profit organization that helps to bring amazing people together to create, develop, design and make things with Open Technologies and share work for the benefit of all. Our project is an important part of Free Software Ecosystem. We work with other free projects to create high-quality solutions that span the entire software stack. While the many Codeuino contributors develop code, smash bugs, write documentation, and help users, the Foundation acts as a guiding hand in the process and provides resources and infrastructure. It steers releases, determines what software is officially part of the project, and acts as an official face of the Codeuino Project to the outside world, through it delegates most of its authority to specialized teams. Therefore, this guide would help you to become an integral part of the Community.</p><br/>
+                <div class="g-heading-v9 g-mb-30 heading-padding">
+                    <h3 class="text-uppercase">Working with Codeuino</h3>
+                </div>
+                <p class="g-fs-16 reduce ta-js">Everyone in the Codeuino community is expected to follow our Code of Conduct. To become part of the Codeuino developers team, there are a few steps you need to complete.</p>
+                
+                <blockquote>
+                    <ul id="points">
+                        <li>
+                            <p class="g-fs-16"><span>1. </span>Read the issues.</p>
+                            <p class="g-fs-16"><span>2. </span>Before Solving, leave a comment suggesting your will.</p>
+                            <p class="g-fs-16"><span>3. </span>After being assigned the issue, solve the same.</p>
+                            <p class="g-fs-16"><span>4. </span>Send the Pull Request to the “master” branch.</p>
+                            <p class="g-fs-16"><span>5. </span>After the merge is successful, review the pull request.</p>
+                        </li>
+                    </ul>
+                </blockquote>
+                <p class="g-fs-16 ta-js">Make sure you follow the steps as mentioned, and hence it will make you familiar with the workflow of the organization. Also, you would successfully be able to contribute to the community.</p>
+                <div class="g-heading-v9 g-mb-30 steps-heading">
+                    <h4 class="text-uppercase">Step 1. Familiarize, Communicate and Get Involved with the Community</h4>
+                </div>
+                <p class="g-fs-16 ta-js">To get started, the first step is to meet the community. We use Slack to communicate, and there the helpful community will guide you. Slack is an instant messaging service used by developers and users of Github. Slack uses chat rooms, where developers can join in and can talk about a particular topic. Codeuino has two types of chat rooms (channels) – <strong>Meeting channel and General channel.</strong><br/><br/>
+                    <strong>Meeting chat room</strong> is related to a specific details and <strong>General chat room</strong> is related to general discussion topics like conferences, workshops and it is here where we welcome Newcomers and guide them. This is the main chat room of Codeuino. Everything related to conferences and off topics are discussed here.</p>
+                <p class="g-fs-16 reduce ta-js">The list of all available chat rooms are available here - channel list. But before joining the community, here are few things that you should keep in mind.</p>
+                
+                <blockquote>
+                    <ul id="points">
+                        <li>
+                            <p class="g-fs-16"><span>1. </span>Only log into Slack using your GitHub account.</p>
+                            <p class="g-fs-16"><span>2. </span>It will be appreciated if your Github username and slack username is same which will make easier for all to identify you.</p>
+                            <p class="g-fs-16"><span>3. </span>Do not mention users unrelated to a particular issue/question. This also means you should not mention the person that was last online. For example, mentioning someone on a Github issue will subscribe them to that issue, even if they don’t want to be a part of the discussion. However, mentioning someone is okay if they really need to see your message.</p>
+                            <p class="g-fs-16"><span>4. </span>Don’t repeatedly mention people in an ongoing conversation.</p>
+                            <p class="g-fs-16"><span>5. </span>You should ask someone before mentioning them.</p>
+                        </li>
+                    </ul>
+                </blockquote>
+                <p class="g-fs-16 ta-js">Now you are ready to join Codeuino community at Codeuino Slack. The newcomers would be greeted by our Slack bot which will invite you to introduce yourself to the team.<br/><br/>
+                    <strong>Note:-</strong>> If you have questions, make sure they are precise and have the proper documentation style. You must feel free to contact us on Slack while you encounter any problem.<br/><br/>
+                    Also, we use <strong>Hangouts</strong>, to conduct our discussions and for video conferencing. So you must have an account on the same. Also, we recommend you that our Organization Team is always there for your help, but irrelevant doubts will create unnecessary confusion during discussions.
+                </p>
+            </div>
+            <div class="g-heading-v9 g-mb-30 steps-heading">
+                <h4 class="text-uppercase">Step 2. Proposing a Project</h4>
+            </div>
+            <p class="g-fs-16 ta-js">If you have some interesting ideas about a new project that you think our organization should work upon, you are most welcome to do so. To propose a new project follow the steps mentioned below :-<br/><br/>
+            </p>
+            <blockquote>
+                <ul id="points">
+                    <li>
+                        <p class="g-fs-16 ta-js"><span>1. </span>We have created a different channel <strong>#chalk-outProposals</strong> on Slack for our organization where you can propose your project. In this channel we have the team which decides the projects that our organization should work upon.</p>
+                        <p class="g-fs-16 ta-js"><span>2. </span>If your project is unanimously accepted by the team then you will be allowed to create issues regarding your project. But until then, you cannot do so.</p>
+                        <p class="g-fs-16 ta-js"><span>3. </span>As soon the project is accepted you will be the official admin of that project and you will be free to create issues. You are allowed to assign the issue to anyone you feel worthy( if the one is interested to do it ). Please do not mention anyone without a reason.</p>
+                        <p class="g-fs-16 ta-js"><span>4. </span>Also, you can then label the issue as <strong>“Work In Progress” (wip)</strong>, so that others do not work on it.</p>
+                    </li>
+                </ul>
+            </blockquote>
+            <div class="g-heading-v9 g-mb-30 steps-heading">
+                <h4 class="text-uppercase">Step 3. Working on the Issue</h4>
+            </div>
+            <p class="g-fs-16 ta-js">Help is always welcome, be it any form, whether it is working on the documentation process or solving an issue. It is the best way to get along with the community. You can view all the issues on our GitHub Organization.<br/><br/>
+                <strong>Important !!</strong> You need to be logged in before you follow the issues link. They will give you a glimpse of what it’s like to work with us and what the workflow is like.<br/><br/>
+                Also, for those who are new to Git and GitHub, our guidelines will help you throughout the process.
+                <strong>Cloning our GitHub repository:-</strong> The git clone command is used to copy an existing Git repository from a server to the local machine. To clone our git repository follow these steps in your terminals:-<br/><br/>
+            </p>
+            <blockquote>
+                <ul id="points">
+                    <li>
+                        <p class="g-fs-16 ta-js"><span>1. </span>cd path where you would like the clone to create a directory
+                        </p>
+                        <p class="g-fs-16 ta-js"><span>2. </span>copy and paste this code into your terminals for cloning our website code. Similarly just change the link to clone the other repositories <strong>git clone https://github.com/Codeuino/website-www.Codeuino.org.git</strong></p>
+                        <p class="g-fs-16 ta-js"><span>3. </span>Now pick an issue which isn’t assigned and which you would like to fix. Leave a comment that you would like to work on that particular issue. This way we don’t have multiple people working on the same issue at the same time. After this, you can start working on it.</p>
+                        <p class="g-fs-16 ta-js"><span>4. </span><strong>Caution:-</strong> You should never work on an issue without being assigned. Also take up the issue, only when you know what the problem is and how to solve it.</p>
+                    </li>
+                </ul>
+            </blockquote>
+            <div class="g-heading-v9 g-mb-30 steps-heading">
+                <h4 class="text-uppercase">Step 4. Forking and Testing the Modifications</h4>
+            </div>
+            <p class="g-fs-16 ta-js">You should first fork the repository by visiting the official repository of Codeuino and clicking on the button Fork. This step is needed only once.<br/><br/>
+                <strong>Important !!</strong> It is important that you DO NOT make your changes on the master branch of your forked repository as you cannot have two pull requests using the same branch name. Therefore, if your fork’s master has been used in a pull request and you decide to work on a different issue you will have to branch eventually. Differently every new commit that you make on your master branch will get attached to the initial pull request and that will result in altering the purpose of that request.<br/><br/>
+                You can create a branch per set of changes. To do that run:
+                <strong>$ git checkout -b branchname</strong> where your branch name can be anything other than master. The scope is your forked repository. The branch name will be shown on pull request.Before pushing the pull request, you must test your changes with some concrete examples.<br/><br/>
+            </p>
+            <div class="g-heading-v9 g-mb-30 steps-heading">
+                <h4 class="text-uppercase">Step 5. Sending a Pull Request</h4>
+            </div>
+            <p class="g-fs-16 ta-js">Now that your commit has been sent to your fork, it is time to create a Pull Request. You can do this by accessing your fork on GitHub and clicking New Pull Request. <br/><br/>
+            <strong>Note:-</strong> Comments are always welcomed, as the readability of the code becomes easy for the other developers. <br/><br/>
+            <strong>Creating a Pull Request:-</strong> If you've forked a repository and made changes to the fork, you can ask that the upstream repository accept your changes by creating a pull request.<br/><br/>
+            <strong>Note:</strong> To open a pull request in a public repository, you must have write access to the head or the source branch or, for organization-owned repositories, you must be a member of the organization that owns the repository to open a pull request. <br/><br/>
+            </p>
+            <blockquote>
+                <ul id="points">
+                    <li>
+                        <p class="g-fs-16"><span>1. </span>Navigate to the original repository you created your fork from.</p>
+                        <p class="g-fs-16"><span>2. </span>To the right of the Branch menu, click<strong> New pull request.</strong></p>
+                        <p class="g-fs-16"><span>3. </span>Type a title and description for your pull request.</p>
+                        <p class="g-fs-16"><span>4. </span>If you do not want to allow anyone with push access to the upstream repository to make changes to your PR, deselect <strong>Allow edits from maintainers.</strong></p>
+                        <p class="g-fs-16"><span>5. </span>Click <strong>Create pull request.</strong></p>
+                    </li>
+                </ul>
+            </blockquote>            
+        </div>
+        </div>
+    );
+}
+
+export default JoinUs;
