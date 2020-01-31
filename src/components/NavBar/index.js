f62380a968b88ac056000c946ee9839846189789
mrkhst <iit2016126@iiita.ac.in>
2020-01-31 20:56:26 +0530

    Fixed the Join Us link

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -48,7 +48,7 @@ const NavBar = () => {
               <a class="nav-link" href="/bloglist">Blogs<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/bloglist">Join Us<span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="/joinus">Join Us<span class="sr-only">(current)</span></a>
             </li>
           </ul>
         </div>
