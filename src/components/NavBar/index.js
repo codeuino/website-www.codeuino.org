b6aed8b1135d1c27d6d77d517e63dcc2e5afb1a4
Rakshit <30564496+rak-shit@users.noreply.github.com>
2020-02-02 16:55:23 +0530

    Fixed the Join Us link (#184)
    * Fixed the Join Us link
    
    * Fixed the page reload issue on routing

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -15,40 +15,40 @@ const NavBar = () => {
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="myNav">
         <div className="container">
-        <a class="navbar-brand" href="/"><Image id="logo" src={logo} alt="codeuino logo" style={{heigth:"20vh",width:"10vw"}}/></a>
+        <Link class="navbar-brand" to="/"><Image id="logo" src={logo} alt="codeuino logo" style={{heigth:"20vh",width:"10vw"}}/></Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+              <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <Link class="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Activities
-              </a>
+              </Link>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="https://docs.codeuino.org/documentation/activities/google-summer-of-code-2019" style={{textAlign : "center"}}>GSOC 2019</a>
-                <a class="dropdown-item" href="https://github.com/codeuino/gci" style={{textAlign : "center"}}>GCI 2019-20</a>
-                <a class="dropdown-item" href="https://kwoc.kossiitkgp.org/" style={{textAlign : "center"}}>KWOC 2018</a>
-                <a class="dropdown-item" href="https://www.gssoc.tech/index.html" style={{textAlign : "center"}}>GSSOC 2019</a>
+                <Link class="dropdown-item" to="https://docs.codeuino.org/documentation/activities/google-summer-of-code-2019" style={{textAlign : "center"}}>GSOC 2019</Link>
+                <Link class="dropdown-item" to="https://github.com/codeuino/gci" style={{textAlign : "center"}}>GCI 2019-20</Link>
+                <Link class="dropdown-item" to="https://kwoc.kossiitkgp.org/" style={{textAlign : "center"}}>KWOC 2018</Link>
+                <Link class="dropdown-item" to="https://www.gssoc.tech/index.html" style={{textAlign : "center"}}>GSSOC 2019</Link>
               </div>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/team">Team<span class="sr-only">(current)</span></a>
+              <Link class="nav-link" to="/team">Team<span class="sr-only">(current)</span></Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="https://docs.codeuino.org/documentation/">Docs<span class="sr-only">(current)</span></a>
+              <Link class="nav-link" to="https://docs.codeuino.org/documentation/">Docs<span class="sr-only">(current)</span></Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/about">About Us<span class="sr-only">(current)</span></a>
+              <Link class="nav-link" to="/about">About Us<span class="sr-only">(current)</span></Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/bloglist">Blogs<span class="sr-only">(current)</span></a>
+              <Link class="nav-link" to="/bloglist">Blogs<span class="sr-only">(current)</span></Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/bloglist">Join Us<span class="sr-only">(current)</span></a>
+              <Link class="nav-link" to="/joinus">Join Us<span class="sr-only">(current)</span></Link>
             </li>
           </ul>
         </div>
