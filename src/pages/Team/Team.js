c036f8ddb617fb08d7548c485a846870c311ff9b
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-07-13 00:38:16 +0530

    improvements to teams page

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
-
-import PropTypes from "prop-types";
-// import {Card,Button} from 'react-bootstrap'
-
-import "./Team.css";
 import BoardMembersWrapper from "./BoardMembers";
 import CoreContributorsWrapper from "./CoreContributors";
 import ScrollToTopBtn from "../../components/ScrollToTopBtn/ScrollToTopBtn.js";
 import axios from "axios";
-import $ from "jquery";
+import "./Team.css";
+
 class Team extends Component {
   constructor(props) {
     super(props);
@@ -17,28 +13,17 @@ class Team extends Component {
       contributors: [],
     };
   }
-  // var jsonURL = "https://s3.ap-south-1.amazonaws.com/pr-webhook-contributors-json/contributors.json";
-  //     $.getJSON(jsonURL, function(json) {
-  //         $.each(json, function() {
-  //             var contributorDiv = "";
-  //             contributorDiv += '<div class="col-xs-1 col-3xs-12 lawyer-post g-mb-50"><a href=' + this.url + '><img class="img-responsive full-width g-mb-25" src="' + this.image + '" alt=""></a> </div>';
-  //             // contributorDiv += '<p class="g-fs-10 text-uppercase center-align">' + this.name + '</p>';
-  //             // contributorDiv += '<a href=' + this.url + ' class="btn-u btn-u-lg btn-u-red btn-u-upper github-button"><i class="fa fa-github"></i> </a> </div>'
-  //             $('#contributors').append(contributorDiv);
-  //         });
-  //     });
+
   componentDidMount() {
     var jsonURL =
       "https://s3.ap-south-1.amazonaws.com/pr-webhook-contributors-json/contributors.json";
     axios.get(jsonURL).then((res) => {
       this.setState({ contributors: res.data });
-      //console.log(this.state);
     });
     window.scrollTo(0, 0);
   }
   render() {
     let contributors = this.state.contributors;
-    // console.log(contributors)
     var contrilist = contributors.map((contri) => {
       return (
         <div class="col-xs-2 lawyer-post g-mb-50 m-4">
